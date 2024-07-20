e381b0172635d28b273d8fa90471827024ca12f2
collajuan <collajuan@gmail.com>
2024-07-20 10:16:50 +0200

    contador

--- JavaScript/bucles/buclces.js
+++ JavaScript/bucles/buclces.js
@@ -26,7 +26,7 @@ for (const prop in profesor) {
 
 // TODO: escribe un bucle que muestre 10 veces un mensaje por la consola con el text en color rojo y azul alternativamente
 for (let i = 0; i < 10; i++) {
-    if (i % 2 == 0) {
+    if (i % 2) {
         console.log('%cMensaje Rojo', 'color: red;');
     } else {
         console.log('%cMensaje Azul', 'color: blue;');
@@ -38,38 +38,40 @@ for (let i = 0; i < 10; i++) {
 
 for (let i = 0; i <= 20; i++) {
     let mensaje = `${i}`
-    if (i % 2 == 0) {
+    if (i % 2) {
         mensaje += " hola"
     }
-    if (i % 3 == 0) {
+    if (i % 3) {
         mensaje += " adeu"
     }
-    if (i % 5 == 0) {
+    if (i % 5) {
         mensaje += " qué tal?"
     }
     console.log(mensaje);
 }
 
-// TODO: fes un contador que mostri a la consola els números de l'1 al número que introdueixis com a paràmentre (fins a 1000 com a màxim)
+// TODO: haz un contador que muestre en la consola los números del 1 al número que introduzcas como parámetro (fins a 1000 com a màxim)
 // let increment = prompt("ingrese numero")
 // for(increment ; increment < 1000; increment++){
 //     console.log(increment);
 // }
 
 // TODO: haz un contador que sólo muestre los números que tengan un dígito contenido en la string definida por el usuario (con prompt) hasta 100
-//let numero = prompt("Ingrese un numero:")
+// let numero = prompt("Ingrese un numero:")
 let numero = "2589"
 contador = []
 for (let char of numero) { //por cada caracter del string del usuario...
     for (let j = 0; j < 100; j++) { //por cada numero del 1 al 100...
         for (let num of j.toString()) { //por cada digito del numero del 1 al 100...
-            if (num == char && !contador.includes(j)) { // Verifico si char esta en el numero y lo agrego si no esta ya incluido por otro caracter
+            if (num == char && !contador.includes(j)) { // Verifico si char está en el numero y lo agrego a contador si no esta ya incluido por otro caracter
                 contador.push(j)
             }
         }
     }
 }
-// console.log(contador);
+console.log(contador);
+
+
 
 
 // TODO: lista de la compra. Haz que un prompt se repita hasta que el usuario introduzca una palabra clave; 
@@ -86,14 +88,14 @@ listaCompra = {}
 item = 0;
 articulo = ''
 while (articulo != "Exit") { 
-    articulo = prompt("Indique Item. Exit para terminar")
+     articulo = prompt("Indique Item. Exit para terminar")
     if (articulo == 'Exit') {
         break;
     }
     listaCompra[`${item}`] = articulo //agrego otro item al objeto
     item++; 
 }
-
+console.log(listaCompra);
 console.log('Lista de Compra:');
 for (const key in listaCompra) {
     console.log("- " + listaCompra[key]);
