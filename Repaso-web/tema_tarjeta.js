0fed9ac493f0a747ef10a0f9a988979ce6b25d79
collajuan <collajuan@gmail.com>
2024-09-27 17:49:43 +0200

    fetch

--- Repaso-web/tema_tarjeta.js
+++ Repaso-web/tema_tarjeta.js
@@ -1,17 +1,61 @@
 
-url = 'https://pokeapi.co/api/v2/pokemon/'
+url = 'https://pruebasalle-a1814-default-rtdb.europe-west1.firebasedatabase.app/'
+project = 'Juan-Collazo/'
+database = 'prueba.json'
+id = 'prueba/-O7o5aZEsz5vbeNFxUF-.json'
 
 
-fetch(url + 160)
+fetch(url + project + id)
+.then(
+    (response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error("Request failed!");
+    })
+.then(res => console.log(res))
+
+
+function postAlumno() {
+    fetch(url + project + id,
+        {
+            method: 'PATCH',
+            body: JSON.stringify({deporte: 'tenis'})
+        }
+    )
     .then(res => res.json())
-    .then(res => console.log(res))
+    .then(res => console.log(res))    
+}
 
-sol = document.querySelector('#sol')
+// fetch(url, {
+//     method: "POST",
+//     body: data,
+//     headers: {
+//         "Content-type": "application/json",
+//         apikey: apiKey,
+//     }
+// })
+// .then(
+//     (response) => {
+//         if (response.ok) {
+//             return response.json();
+//         }
+//         throw new Error("Request failed!");
+//     },
+//     (networkError) => console.log(networkError.message)
+// )
+// .then(res => console.log(res))
 
 
-sol.addEventListener("click", () => {
-    console.log('entra sol');
-        
-    adudio = new Audio('https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/132.ogg')
-    adudio.play()
-});
+
+
+
+// sol.addEventListener("click", () => {
+//     numero = Math.floor(Math.random()*150)
+//     linkAudio = 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/' + numero + '.ogg'
+//     console.log('entra sol');
+
+//     adudio = new Audio(linkAudio)
+//     adudio.play()
+// });
+
